89449106d41ef8e6445fedf6b3e9a9e3126e1e01
jorgescalona <escajorge@gmail.com>
2016-02-08 14:37:40 -0430

    Confirmacion inicial

---
+++ README.md
@@ -0,0 +1,38 @@
+# convermkv
+
+El presente script en python toma una ruta dada y convierte los archivos .mkv
+a .avi mediante mencoder (por lo tanto se debe instalar previamente mplayer o 
+mencoder). Esto con la finalidad de reproducir en aparatos antiguos que no soportan
+el formato mkv.
+
+El mismo renombra los archivos para eliminar los espacios entre nombres y luego
+ejecuta de manera ordenada la conversión de los archivos de la ruta dada o en su 
+defecto si no se proporciona ruta sino enter el directorio actual.
+
+
+Proxima Version:
+
+Se espera preparar el script para convertir a otros formatos.
+
+
+@jorgescalona  2016 
+
+------------------------------------------------------------------------
+
+
+The present python script takes a given path and converts .mkv files
+to .avi using mencoder (therefore you must first install or mplayer
+mencoder). This in order to play on older devices that do not support
+mkv format.
+
+It renames the files to remove the spaces between names and then
+running orderly converting files or the path given its
+default if path is not provided, but enter the current directory.
+
+
+Next Version:
+
+It is expected to prepare the script to convert to other formats.
+
+
+@jorgescalona 2016
---
+++ convermkv.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+
+def despedida():
+    """Mensaje de ejecucion exitosa"""
+    print 'El programa se ejecuto de manera exitosa \n'
+    print '@jorgemustaine, https://github.com/jorgescalona'
+
+def renom(arch):
+    """esta funcion elimina los espacios en blacos de los nombres de archivos"""
+    print 'EN LA FUNCION'
+    print arch
+    sesp=''.join(arch.split())
+    return sesp
+
+def ord_direc():
+    """Funcion que ordena el directorio dado y hace la convercion mediante mencoder"""
+    indice = 1
+    control = 1
+
+    while True:
+        if control==1:
+            directorio=raw_input('Ingrese la ruta de la carpeta: ')
+            if directorio=='':
+                directorio=os.getcwd()
+                print directorio
+            control=2
+        try:
+            lista=os.listdir(directorio)
+            l=sorted(lista)
+            for i in l:
+            if i[-4:] == ".mkv":
+                    org=renom(i)
+                    tran=org[0:-3:]
+                    nn=''
+                    final=tran+'avi'
+                    os.rename(i,org)
+                    os.system('mencoder %s -ovc xvid -oac mp3lame -oac mp3lame -nosub -lameopts abr:br=192 -xvidencopts pass=2:bitrate=-700000 -o %s' % (org,final))
+                    indice=indice+1
+            despedida()
+            break
+
+        except:
+            print 'Ingrese un path valido'
+            control=1
+            continue
+
+ord_direc()
